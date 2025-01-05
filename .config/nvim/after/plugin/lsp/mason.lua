e69d34750f801686f447d921e1d225f243dc0e5a
Kay <kkhan1991@hotmail.com>
2025-01-05 13:34:09 +0000

    updates

--- .config/nvim/after/plugin/lsp/mason.lua
+++ .config/nvim/after/plugin/lsp/mason.lua
@@ -20,7 +20,7 @@ mason.setup()
 mason_lspconfig.setup({
 	-- list of servers for mason to install
 	ensure_installed = {
-		"tsserver",
+		"ts_ls",
 		"lua_ls",
 		"pyright",
 		"jsonls",
@@ -32,7 +32,7 @@ mason_lspconfig.setup({
 		"cssls",
 		"tailwindcss",
 		"yamlls",
-        "gopls"
+		"gopls",
 	},
 	-- auto-install configured servers (with lspconfig)
 	--automatic_installation = true, -- not the same as ensure_installed
@@ -43,9 +43,9 @@ mason_null_ls.setup({
 	ensure_installed = {
 		"prettier", -- ts/js formatter
 		"stylua", -- lua formatter
-        "eslint",
-        "eslint_d",
-        "prettierd"
+		"eslint",
+		"eslint_d",
+		"prettierd",
 	},
 	-- auto-install configured formatters & linters (with null-ls)
 	--automatic_installation = true,
--- .config/nvim/lua/kay/maps.lua
+++ .config/nvim/lua/kay/maps.lua
@@ -23,3 +23,11 @@ vim.api.nvim_set_keymap('n', '<M-j>', ')', {noremap = true, silent = true})
 
 -- Map Alt+K to move to the previous sentence
 vim.api.nvim_set_keymap('n', '<M-k>', '(', {noremap = true, silent = true})
+
+
+-- Navigate between buffers
+vim.api.nvim_set_keymap('n', '<leader>n', ':bnext<CR>', { noremap = true, silent = true })   -- Next buffer
+vim.api.nvim_set_keymap('n', '<leader>p', ':bprevious<CR>', { noremap = true, silent = true }) -- Previous buffer
+
+-- Close the current buffer
+vim.api.nvim_set_keymap('n', '<leader>c', ':bd<CR>', { noremap = true, silent = true })       -- Close buffer
