d36793056965dbb7f5b253af87038cb93414edb1
Ubuntu <ubuntu@ip-172-31-31-152.eu-west-2.compute.internal>
2026-07-04 18:42:30 +0000

    feat

--- dependencies.sh
+++ dependencies.sh
@@ -1,45 +1,101 @@
-# Init
-sudo apt-get install curl git -y 
-sudo apt install gnome-tweaks 
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Core packages
+sudo apt-get update
+sudo apt-get install -y \
+  curl \
+  git \
+  ca-certificates \
+  build-essential \
+  procps \
+  file \
+  make \
+  wget \
+  xclip \
+  python3-pip
 
 # ZSH
-sudo apt install zsh -y
+sudo apt-get install -y zsh
 
 # oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+  "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+  "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 # nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+
+NVM_LATEST_TAG="$(
+  git ls-remote --tags --refs https://github.com/nvm-sh/nvm.git 'v*' \
+    | awk -F/ '{print $3}' \
+    | sort -V \
+    | tail -n1
+)"
+
+curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_LATEST_TAG}/install.sh" | bash
+
+# Load nvm for this script run
+. "$NVM_DIR/nvm.sh"
+
 nvm install --lts
-npm install -g neovim eslint_d npm-check-updates prettier typescript-language-server typescript vscode-langservers-extracted yarn @prisma/language-server serverless vim-language-server
-#source ~/.zshrc
+nvm use --lts
+nvm alias default 'lts/*'
+
+npm install -g \
+  neovim \
+  eslint_d \
+  npm-check-updates \
+  prettier \
+  typescript-language-server \
+  typescript \
+  vscode-langservers-extracted \
+  yarn \
+  @prisma/language-server \
+  serverless \
+  vim-language-server
 
 # tmux
-sudo apt-get install tmux xclip
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm # install plugins
-tmux && ctrl + b  + (shift + I)
+sudo apt-get install -y tmux
 
-#homebrew
+git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+
+# After this script finishes, open tmux and press:
+# Ctrl+b, then Shift+i
+
+# homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-#pyenv
-sudo apt update; sudo apt install make build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# pyenv
+sudo apt-get install -y \
+  libssl-dev \
+  zlib1g-dev \
+  libbz2-dev \
+  libreadline-dev \
+  libsqlite3-dev \
+  libncursesw5-dev \
+  xz-utils \
+  tk-dev \
+  libxml2-dev \
+  libxmlsec1-dev \
+  libffi-dev \
+  liblzma-dev \
+  libzstd-dev
 
 brew install pyenv
 
-#nvim
+# nvim
 brew install neovim
-# neovim vimplug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 brew install efm-langserver
-python -m pip install --user --upgrade pynvim
-sudo apt-get install ripgrep # telescope
-sudo apt install fd-find # telescope
-# install neovim plugins
+
+python3 -m pip install --user --upgrade pynvim
+
+sudo apt-get install -y \
+  ripgrep \
+  fd-find
